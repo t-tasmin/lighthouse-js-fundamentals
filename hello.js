26d4ce4401974a1e0f466aa44e8da42a3cacf153
Tania Tasmin <taniat.kaau@gmail.com>
2021-09-24 18:35:16 +0000

    Change the named function to an anonymous function expression with a parameter

--- hello.js
+++ hello.js
@@ -1,5 +1,7 @@
-const sayHello  = function () {
-  console.log("Hello, world");
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
 }
 
-sayHello();
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
