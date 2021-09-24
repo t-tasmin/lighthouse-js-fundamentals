942ef9ce49ee1380194ff59cd079f7d64ee37ae2
Tania Tasmin <taniat.kaau@gmail.com>
2021-09-24 12:00:01 +0000

    Add more conditional examples with logical operators in conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,20 @@
-const temperature = 12;
+// Example of and operator
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
 }
 
-console.log("Now you're ready to go outside!");
+// Example of or operator
+const temperature = 12;
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+// Example of not operator
+const raining=false;
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
