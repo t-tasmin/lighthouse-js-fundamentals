6288fe553aa22789f7fbf04b78b53d26f4737d28
Tania Tasmin <taniat.kaau@gmail.com>
2021-09-27 02:43:26 +0000

    Use a function to merge and sort two arrays

---
+++ mergearrays.js
@@ -0,0 +1,13 @@
+function merge(A, B)
+{
+  for (let i = 0; i < B.length ; i++ )
+  {
+    A.push( B[i]);
+  }
+
+  return A.sort();
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
