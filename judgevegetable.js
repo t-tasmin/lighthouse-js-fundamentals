81d3069572e2f0d3225a40ee89e4baf58f63d8de
Tania Tasmin <taniat.kaau@gmail.com>
2021-09-27 16:25:44 +0000

    Use a function to determine submitter of best vegetable

---
+++ judgevegetable.js
@@ -0,0 +1,41 @@
+function judgeVegetable(vegetables, metric)
+{
+  let max = vegetables[0][metric];
+  
+
+  let maxindex = 0;
+
+  for (let i = 0; i < vegetables.length; i++ )
+  {
+    if(vegetables[i][metric] > max)
+    {
+      max = vegetables[i][metric];
+      maxindex = i;
+    }
+
+  }
+
+  return vegetables[maxindex].submitter;
+}
+
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'plumpness';
+
+console.log(judgeVegetable(vegetables, metric));
